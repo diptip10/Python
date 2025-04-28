18e5b0b0d0d7437890e82d08faff8c11d26288ed
dipti10@gmail.com <dipti@12345>
2025-04-28 15:50:25 +0530

    python code

--- a1.py
+++ a1.py
@@ -2,6 +2,10 @@ print("Pawar")
 
 #  find a sum from the given list
 list=["2,3,4,5,6"]
-list1=list[0].split(",")
-# for i in list:
-#     print(int(list))
+list1=list[0].split(',')
+
+total = 0
+for i in list:
+    total =total + int(i)
+
+print(total)
