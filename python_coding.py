02055bcf99a88014358b05c19de222b23db39017
dipti <diptipawar499@gmail.com>
2025-06-14 16:40:21 +0530

    python practice

---
+++ python_coding.py
@@ -0,0 +1,16 @@
+# print the statement - "python uses \n for newline"
+print("python uses \n for newline")
+
+# how will you print "/\/\/\/\"
+print("/\\/\\/\\/\\")
+
+# slicing
+str = 'python coding'
+print(str[2:10:2])
+
+#merge three lists
+x = [2,3,4]
+y = [5,6,7]
+z = [8,9,1]
+print(list(zip(x,y,z)))
+print(x+y+z)
