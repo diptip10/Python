51da254b3d98f21ae711ca370a1c9eabae7cde06
dipti <diptipawar499@gmail.com>
2025-06-14 17:19:35 +0530

    updated file

--- python_coding.py
+++ python_coding.py
@@ -14,3 +14,13 @@ y = [5,6,7]
 z = [8,9,1]
 print(list(zip(x,y,z)))
 print(x+y+z)
+
+# reverse the words from the string
+str="Dipti Anil Pawar"
+# split the string
+str1=str.split()
+# reversing
+str1=str1[::-1]
+# use .join() 
+str2= " ".join(str1)
+print(str2)
