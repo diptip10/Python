f9d0625ec2ca2c401603d82944d62c26b4f90f74
dipti <diptipawar499@gmail.com>
2025-06-30 23:27:10 +0530

    python codes

--- flow_control_statements.py
+++ flow_control_statements.py
@@ -1,1 +1,48 @@
-# conditional statements
+## flow control statements
+# 1)conditional statements -- if, if-else, elif [based on specific condition specific block is executed]
+a = 10
+if a==10:
+    print('if condition')
+elif a==20:
+    print('elif condition')    
+else:
+    print('else condition')    
+
+
+# 2)iteration statements-- fpr and while[same code is repeated multiple times] 
+# for - when we have starting value or ending value increment or decrement
+# while- to check whether the data is available or not
+for x in range(3):
+    print('for loop')
+
+# in 3 cases else block is not executed
+# case1: whenever their is break statement in for loop
+for x in range(20):
+    if x==10:
+        break
+    print('python', x)
+else:
+    print('normal execution for for loop')
+
+# case2: whenever we get an exception in for loop
+for x in range(10):
+    print('for loop',x)
+    print(10/0)
+else:
+    print("normal execution of for loop")
+
+# case3: when virtual machine / programme is shut down
+import os
+for x in range(10):
+    print('python', x)
+    os._exit(0)
+else:
+    print("normal execution of for loop")    
+
+# while loop
+a = 0
+while a<10:
+    print('dipti')
+    a=a+1
+    print("normal code")
+
