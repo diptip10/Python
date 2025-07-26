f4de8d557925a7328f10a264785a15b175bcd025
dipti <diptipawar499@gmail.com>
2025-07-26 15:50:47 +0530

    coding

--- datatypes.py
+++ datatypes.py
@@ -115,4 +115,37 @@ while num <= 101:
     print(sum_of_numbers)
     num+=2
 
+# write a program that asks the user for number N and says whether it is prime or not
+def is_prime(n):
+    if n<=1:
+        return False
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return False
+    return True
+
+# given list
+list = [1,2,3,4,5,6]
+for n in list:
+    if is_prime(n):
+        print('prime',n)    
+
+
+# get the prime numbers from 1 to 30
+n=30
+def is_prime(num):
+    if num<=1:
+        return False
+    for i in range(2,int(num**0.5)+1):
+        if num%i==0:
+            return False
+    return True
+
+for x in range(n+1):
+    if is_prime(x):
+        print('prime',x)
+    
+
+
+
 
