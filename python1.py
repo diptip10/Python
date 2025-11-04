2cf8779da5c72b84535f9c2c26b2181d538ba9de
dipti <diptipawar499@gmail.com>
2025-11-04 22:27:34 +0530

    python basics

---
+++ python1.py
@@ -0,0 +1,33 @@
+'''
+Python basics 
+
+
+Python is a general-purpose programming language created by Guido van Rossum in 1991. 
+It emphasizes readability and simplicity, 
+making it easy for beginners to learn and powerful enough for experts to build complex systems.
+
+Key Features :
+1. Easy to Read and Write - Python's syntax is simple and close to English.
+
+2. Interpreted Language - You don't need to compile your code; Python runs it directly.
+
+3. Dynamically Typed - You don't need to declare variable types explicitly.
+
+4. Extensive Libraries - Python has huge libraries for almost everything:
+
+            Data analysis: pandas, numpy
+
+            Machine learning: scikit-learn, tensorflow
+
+            Web development: Django, Flask
+
+            Automation: os, selenium
+
+5. Cross-Platform - Works on Windows, macOS, and Linux.
+
+6. Object-Oriented - Supports OOP concepts like classes and inheritance.
+
+7. Open Source - Free to use and has a large community for support.
+
+
+'''
