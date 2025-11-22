dc303e335119d8c314d0dc58b20dd134af3710ad
dipti <diptipawar499@gmail.com>
2025-11-22 16:39:30 +0530

    python datatypes

---
+++ python_basics.py
@@ -0,0 +1,45 @@
+"""
+Python- developed by Guido van Rossum in 1991.
+
+# Key points about Python (easy to remember)
+
+1. High-level and beginner-friendly
+
+2. Object-oriented and interpreted language
+
+3. Portable and open-source
+
+4. Huge ecosystem of libraries (pandas, NumPy, PySpark, requests, Flask, etc.)
+
+5. Commonly used for data engineering, automation, AI/ML, backend APIs, and scripting
+
+
+# Comments in Python are non-executable lines used to explain the code, improve readability, 
+and help other developers understand the logic.
+
+1. single line- Starts with #
+2. multi line- Written using triple quotes ("""   """ ) or ('''  ''')
+
+"""
+
+# keywords
+import keyword
+print(keyword.kwlist)
+
+# Datatypes in python (general)- Python is dynamically typed language
+
+# 1. Number - integer(int) 10,20
+#             float 10.4, 20.5
+#             complex (10+j)
+# 2. String- str- 'Python', 'data'
+# Boolean- bool - True(1), False (0)
+
+eid = 1    # int
+ename ='Riya'  # str
+esal = 10000.45   # float
+
+# To check the type of variable we have type() function
+print(type(eid))
+print(type(ename))
+print(type(esal))
+
