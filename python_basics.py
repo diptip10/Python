fc664cf94b1cd491c091c8ef7ace928929eccf1a
dipti <diptipawar499@gmail.com>
2025-11-24 15:27:09 +0530

    for loop

--- python_basics.py
+++ python_basics.py
@@ -132,7 +132,7 @@ print("eid={0} ename={2} esal={1}".format(eid,ename,esal))
 # 2) Interation - 1. for  2. while
 # 3) transfer - 1. break  2. continue
 
-# CONDITIONAL STATEMENT
+## CONDITIONAL STATEMENT
 
 # case 1 :   if-else condition
 a=10
@@ -164,3 +164,92 @@ print("if body") if True else print("else body")  # single line code
 
 # case 6 - You should take multiple statements in braces {}
 {print("python"),print("code")} if 10<20 else {print("else body"), print("else code")}
+
+
+# elif condition -- (It is nothing but else if) --else is optional here
+a=10
+if a==10:
+    print("hello")
+elif a==20:
+    print("good day")
+elif a==30:
+    print("hii")
+else:
+    print("nice day")
+
+
+## ITERATION STATEMENTS
+
+# range function
+print(list(range(10)))
+print(list(range(4,10)))
+print(list(range(4,10,3)))
+print(list(range(10,4,-3)))
+print(list(range(-10,-5)))
+print(list(range(-10,-5,2)))
+print(list(range(-5,-10,-2)))
+
+# FOR LOOP: A for loop is used when you want to repeat a block of code for each item in a 
+# sequence such as: range(), list, tuple, string, dictionary, set, file
+
+#   CASE 1 
+for x in range(10):
+    print("python",x)
+
+for x in range(4,10):
+    print("python",x)
+
+for x in range(4,10,3):
+    print("python",x)
+
+for x in range(10,4,-3):
+    print("python",x)
+
+for x in range(-10,-4):
+    print("python",x)
+
+for x in range(-10,-4,3):
+    print("python",x)
+
+for x in range(-4,-10,-3):
+    print("python",x)
+
+L=[10,20,30]
+for x in L:
+    print(x)
+
+t=(10,20,30)
+for x in t:
+    print(x)
+
+# CASE 2
+for x in range(10):
+    print("hello",x)
+else:
+    print("normal execution of for loop")
+
+# in 3 cases else block is not executed
+#case1 - Whenever in the for loop there is break statement, else block will not execute
+#ex.
+for x in range(10):
+    if x == 3:
+        break
+    print("hello",x)
+else:
+    print("normal execution of for loop")
+
+#case2 - Whenever you get exceptions in for loop
+for x in range(10):
+    print("hello",x)
+    print(10/0)            # zero division error
+else:
+    print("normal execution of for loop")
+
+#case3 - when virtual machine/ programme is shut down
+import os
+for x in range(10):
+    print("hii",x)
+    os._exit(0)
+else:
+    ("normal execution of for loop")
+
